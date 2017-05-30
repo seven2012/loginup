776359a05ae6dd374488f5f9d75afa8df71a74f6
july77 <ljlpsy11@gmail.com>
2017-05-30 17:27:50 +0800

    登录注册

---
+++ all.js
@@ -0,0 +1,37 @@
+
+sign_login.onclick = function(){
+    login_page.removeAttribute('hidden')
+    signpage.style.display='none';
+
+    let wrap = document.querySelector('.wrapper')
+    wrap.className = 'change'    
+    
+}
+
+
+
+sign_logup.onclick = function(){
+    logup_page.removeAttribute('hidden')
+    signpage.style.display='none'
+
+   let wrap = document.querySelector('.wrapper')
+    wrap.className = 'change'
+    
+}
+
+
+let logupHref = document.querySelector('.logup_page a')
+   logupHref.onclick = function(){
+       login_page.removeAttribute('hidden')
+       logup_page.style.display = 'none'
+       
+       window.location.reload()
+   }
+   
+let loginHref = document.querySelector('.tologup')
+    loginHref.onclick = function(){
+        logup_page.removeAttribute('hidden')
+        login_page.style.display = 'none'
+       
+       window.location.reload()
+    }
